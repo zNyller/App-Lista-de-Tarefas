0e8c0d1c651a0b0cf43e05b05779712bf91c9982
Nyller <edu.sampaio1777@gmail.com>
2022-09-19 10:45:20 -0300

    Commit inicial

---
+++ app/src/main/java/com/nyller/projeto2ad/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.nyller.projeto2ad
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
